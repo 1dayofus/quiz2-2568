42059620705bd14e4f4e4a03c0ee70d7bb309085
Your Name <1day@MacBook-Pro-khxng-Wasu.local>
2025-09-08 11:16:18 +0700

    Third

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { type FooterProps } from "/Users/1day/Desktop/CMU/CPE/quiz2-2568/libs/Footer.ts";
-import { Text, Group, type AppShellFooterProps, type ComboboxFooterProps } from "@mantine/core";
+import { Text, Group, } from "@mantine/core";
 export default function Footer({year, fullName, studentId }: FooterProps) {
   return (
     <Group p="md" justify="center">
